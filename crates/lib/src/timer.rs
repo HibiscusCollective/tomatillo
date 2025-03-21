4faa4b062413ba5fce7ac56a505a296b9ed422ef
Pierre Fouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-03-21 21:06:03 +0800

    refactor(lib/timer): simplify timer by using a single broadcast channel instead of two watchers

--- crates/lib/src/timer.rs
+++ crates/lib/src/timer.rs
@@ -1,15 +1,12 @@
-use std::sync::RwLock;
-
 use tokio::{
-    sync::watch::{self, Receiver, Sender},
-    time::{self, Duration, Instant, Interval},
+    sync::broadcast::{self, Receiver, Sender},
+    time::{self, Duration, Interval},
 };
 
 /// A timer that counts down from a specified duration.
 pub struct Countdown {
     duration: Duration,
-    timer: RwLock<Interval>,
-    tick_instant_tx: Sender<Instant>,
+    interval: Interval,
     time_left_tx: Sender<Duration>,
 }
 
@@ -34,47 +31,26 @@ impl Countdown {
     ///
     /// A new [`Timer`].
     pub fn new(duration: Duration, interval: Duration) -> Self {
-        let (tick_instant_tx, _) = watch::channel(Instant::now());
-        let (time_left_tx, _) = watch::channel(duration);
-        Self {
-            timer: RwLock::new(time::interval(interval)),
-            tick_instant_tx,
-            time_left_tx,
-            duration,
-        }
+        // TODO: Validate inputs. Fail if interval > duration || interval == 0 || duration cannot be more than 1 day (86_400_000ms) u32
+
+        let (time_left_tx, _) = broadcast::channel::<Duration>(1);
+        Self { interval: time::interval(interval), time_left_tx, duration }
     }
 
     /// Starts the countdown.
     pub async fn start(&mut self) {
-        let mut instant_rx = self.tick_instant_tx.subscribe();
-        let mut time_left = self.duration.clone();
+        let intervals = (self.duration.as_secs_f64() / self.interval.period().as_secs_f64()).ceil() as u32;
 
-        while !time_left.is_zero() {
-            time_left -= self.tick(&mut instant_rx).await;
+        for i in (0..=intervals).rev() {
+            self.interval.tick().await;
 
-            self.time_left_tx.send_if_modified(try_set(&time_left));
+            self.time_left_tx.send(self.interval.period() * i).unwrap(); // TODO: Handle failed send
         }
     }
 
     /// Watches the remaining time.
     pub fn watch(&self) -> Receiver<Duration> {
         self.time_left_tx.subscribe()
-    }
-
-    async fn tick(&mut self, instant_rx: &mut Receiver<Instant>) -> Duration {
-        let last_instant = instant_rx.borrow_and_update();
-        last_instant.duration_since(self.timer.write().unwrap().tick().await)
-    }
-}
-
-fn try_set<T: Clone + PartialEq>(new: &T) -> impl FnMut(&mut T) -> bool {
-    move |val| {
-        if val == new {
-            return false;
-        }
-
-        *val = new.clone();
-        true
     }
 }
 
@@ -97,16 +73,8 @@ mod tests {
         let countdown_handle = timer.start();
         let watch_handle = tokio::spawn(async move {
             for (i, expect) in expectations.iter().enumerate() {
-                rx.changed().await.unwrap();
-                let actual = rx.borrow_and_update().as_millis();
-                assert_eq!(
-                    expect.as_millis(),
-                    actual,
-                    "Interval {} expected {}, but got {}",
-                    i,
-                    expect.as_millis(),
-                    actual
-                );
+                let actual = rx.recv().await.expect("Failed to receive duration").as_millis();
+                assert_eq!(expect.as_millis(), actual, "Interval {} expected {}, but got {}", i, expect.as_millis(), actual);
             }
         });
 
@@ -121,8 +89,7 @@ mod tests {
 
         let countdown_handle = timer.start();
         let watch_handle = tokio::spawn(async move {
-            rx.changed().await.unwrap();
-            let actual = rx.borrow_and_update().as_millis();
+            let actual = rx.recv().await.expect("Failed to receive duration").as_millis();
             assert_eq!(0u128, actual);
         });
 
